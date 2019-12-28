70704f41217de0bbde93e8af13db3fd556bb380f
Christopher Willis <willisc9@msu.edu>
2019-12-28 18:50:52 -0500

    working optimization

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,39 +1,32 @@
 import numpy as np
-from scipy import optimize
+import pandas as pd
 
+from scipy import optimize
 from scipy.optimize import minimize, rosen, rosen_der
 
 class Logistic_Regression():
 
 	def __init__(self):
-		# model hyper parameters
-		self.inputLayerSize = 3
-		#self.outputLayerSize = 1
-		# model parameters
-		self.W = np.random.randn(self.inputLayerSize + 1) # include bias term
+		# set model hyper parameters: 
+		# initialize weights with gaussian random numbers, include bias term
+		self.inputLayerSize = 3 
+		self.W = np.random.randn(self.inputLayerSize + 1) # 1D array
 
 	def parameters(self):
 		print(" input size: {}\n output size: {}\n weights: {}\n".format(
 			self.inputLayerSize, self.W))
 
-	# 2x3 * 1x3
-	# 2x4 * 1x4
-
-	# push input through network
-	# vector in scalar out (matrix in vector out)
+	# propagate input through the network
+	# vector in scalar out (or feature matrix in vector out)
 	def forward(self, X):
-		ones = np.ones([X.shape[0],1])
-		X = np.append(ones, X, axis=1)
-		#print('W', self.W)
-		#print('b', self.b)
-		#print('X', X)
-		#print('XW', np.dot(X, self.W.T))
-		#print('XW+b', np.dot(X, self.W.T) + self.b) # this is broadcasting
-		#print('yhat', self.sigmoid(np.dot(X, self.W.T) + self.b))
-		#yhat = self.sigmoid(np.dot(X, self.W) + self.b)
-		#return self.sigmoid(np.dot(X, self.W.T) + self.b)
-		return self.sigmoid(np.dot(X, self.W.T))
+		ones = np.ones([X.shape[0], 1])
+		X = np.append(ones, X, axis = 1)
+		#print('X: {} \nX.shape {}:'.format(X, X.shape))
+		#print('W: {} \nW.shape {}:'.format(self.W, self.W.shape))
+		#print('WXT', np.matmul(self.W, X.T))
+		return self.sigmoid(np.matmul(self.W, X.T)) # I think it goes like [1,k] x [n,k].T ~ [1,n]
 
+	# compute sigmoid function
 	def sigmoid(self, z):
 		return 1 / (1 + np.exp(-z))
 
@@ -41,37 +34,40 @@ class Logistic_Regression():
 	def sigmoidPrime(self, z):
 		return self.sigmoid(z) * (1 - self.sigmoid(z))
 
+	# mean squared error loss
 	#def costFunction(self, X, y):
 	#	yhat = self.forward(X)
-	#	return 1/2 * np.sum((yhat - y) ** 2)
+	#   N = X.shape[0]
+	#	return 1/(2*N) * np.sum((yhat - y) ** 2)
 
 	# binary cross entropy loss
+	# return single scalar value from vector of targets and predictions
 	def costFunction(self, X, y):
 		yhat = self.forward(X)
-		#print('y:', y, y.shape)
-		#print('X:', X, X.shape)
-		print(y.T)
-		print(yhat)
-		print(np.dot(y.T, yhat))
-		#print(np.dot(y.T, np.log(self.forward(X))) + np.dot(1-y.T, np.log(1-self.forward(X))))
-		return np.matmul(y.T, np.log(yhat)) + np.matmul(1-y.T, np.log(1-yhat))
+		N = X.shape[0]
+		#print('y: ', y)
+		#print('yT: ', y.T)
+		#print('yhat: ', yhat)
+		#print('1-yT: ', 1-y.T)
+		#print('1-yhat: ', 1-yhat)
+		return -1/N * (np.matmul(y.T, np.log(yhat)) + np.matmul(1-y.T, np.log(1-yhat)))
 
-	# compute gradient of cost function wrt W and b
-	# should have same number of elements as cols j in X
+	# compute gradient of cost function wrt W and b parameters (i.e. 4 params here)
+	# will have same number of elements as columns k in X
 	def costFunctionGradient(self, X, y):
-		yhat = self.forward(X) # do this first
+		yhat = self.forward(X)
 		ones = np.ones([X.shape[0], 1])
 		X = np.append(ones, X, axis = 1)
-		#print(np.dot((yhat - y).T, X))
-		return 1/len(X) * np.dot((yhat-y).T, X)
+		N = X.shape[0]
+		return 1/N * np.matmul((yhat - y).T, X) # [n,1].T x [n,k] ~ [1,k]
+		# negative here?
 
 	def getParameters(self):
-		#print(self.W.ravel())
 		return self.W
 
 	def setParameters(self, params):
-		pass
 		#self.W = np.reshape(params[0:self.inputLayerSize+1])
+		self.W = params
 
 	#def setParameters(self, params):
 	#	pass
@@ -79,62 +75,41 @@ class Logistic_Regression():
 		#W_end = self.hiddenLayerSize * self.inputLayerSize
 		#self.W = np.reshape(params[W1_start:W1_end])
 
-    #def computeGradients(self, X, y):
-    #    grad = self.costFunctionGradient(X, y)
-    #    return grad.ravel()
-
 	def _cost(self, x, *args):
 		X, y = args[0], args[1]
 		self.setParameters(x)
 		cost = self.costFunction(X, y)
 		print('cost', cost)
-		return cost[0] # how to remove this [0] ???
+		#print('W', self.W)
+		return cost
 
 	def _grad(self, x, *args):
 		X, y = args[0], args[1]
 		self.setParameters(x)
 		grad = self.costFunctionGradient(X, y)
-		print('grad', grad)
-		return grad[0] # same ???
-
-	# def cost_grad(self, x, *args):
-	# 	print('x:', x)
-	# 	X = args[0]
-	# 	print('X:', X)
-	# 	y = args[1]
-	# 	print('y:', y)
-	# 	self.setParameters(x)
-	# 	print(self.getParameters())
-	# 	cost = self.costFunction(X, y)
-	# 	print('cost', cost)
-	# 	grad = self.costFunctionGradient(X, y)
-	# 	print('grad', grad)
-	# 	return cost, grad
+		#print('grad', grad)
+		return grad
 
 	def train(self, X, y):
 
-		params = self.getParameters()
-		#print('params:', params, params.shape)
-
-		# Initial guess. Array of real elements of size (n,),
-		# where ‘n’ is the number of independent variables
+		# initial pamameter guess. Array of real elements of size (n,)
 		x0 = self.W
 		print('x0:', x0, x0.shape)
-		#x0 = 1
 
 		# If jac is a Boolean and is True, 
 		# fun is assumed to return the gradient along with the objective function
-		res = optimize.minimize(self._cost, 
-			x0,
+		res = optimize.minimize(fun = self._cost, 
+			x0 = x0,
 			args = (X, y),
 			jac = self._grad, 
 			method = 'BFGS', 
 			options = {'maxiter' : 1000, 'disp' : True})
+				
+		# update parameters with the optimized result
+		#print('parameters: ', res.x)
+		self.setParameters(res.x)
 
-		print(res.x)
-		#setParameters(res.x)
-
-		return res.success
+		return res
 
 
 
@@ -143,44 +118,42 @@ class Logistic_Regression():
 
 if __name__ == '__main__':
 
+	#data = pd.read_csv('data/HTRU_2.csv', header = None)
+	#x = data[[0,1,2,3,4,5,6,7]].values # normalize this...
+	#y = data[[8]].values
+
+	# use iris without 3rd class! make binary!
+
 	lr = Logistic_Regression()
 	#lr.parameters()
 
-	# two examples
-	#x = np.array([[-1, -2, 3], 
-	#			  [ 1,  1, 1]]).reshape(2,-1)
-	x = np.array([-1, -2, 3]).reshape(1,-1)
-	print('X:', x)
+	# feature matrix X
+	x = np.array([[1.0, 2.0, 3.0], 
+				  [1.1, 2.2, 3.3],
+				  [0.9, 1.9, 2.9],
+				  [-1, -2, -3]])
+	#x = np.array([1, 2, 3])
+	#x.shape = (3, 3)
+	print('X: {} \nX.shape {}:'.format(x, x.shape))
 
+	# predict X
 	yhat = lr.forward(x)
 	print('yhat:', yhat)
-	print('end forward')
 
-	# targets	
-	#y = np.array([1, 
-	#			  0]).reshape(2,-1)
-	y = np.array([1]).reshape(1,-1)
-	print('y:', y)
+	# target labels y
+	y = np.array([1, 1, 1, 0])
+	#y.shape = (2, 1)
+	print('y: {} \ny.shape {}:'.format(y, y.shape))
 
-	print('cool')
-
+	# compute cost function
 	J = lr.costFunction(x, y)
-	#print(J)
+	print('cost: ', J)
 
 	dJ = lr.costFunctionGradient(x, y)
-	#print(dJ)
+	print('grad: ', dJ)
 
-
-	#params = [1,1,1,1]
-	#q = lr.cost_grad(params, x, y)
-	#print(q)
-
-	lr.train(x, y)
-
-
-#res = minimize()
-
-
+	result = lr.train(x, y)
+	print('result: ', result)
 
 
 
